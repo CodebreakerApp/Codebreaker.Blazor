5382f6147b1134c3ca104a857d46171235168210
Sebastian <10291845+szv@users.noreply.github.com>
2022-09-29 02:05:08 +0200

    Moved "Data" from API-project to seperate library-project

---
+++ src/shared/CodeBreaker.Shared/Exceptions/GameNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeBreaker.Shared.Exceptions
+{
+    public class GameNotFoundException : NotFoundException
+    {
+        public GameNotFoundException()
+        {
+        }
+
+        public GameNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public GameNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
