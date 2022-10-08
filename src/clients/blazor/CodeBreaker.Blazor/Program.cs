e8989931d8f9424eb96007590434b96e4445efff
Sebastian <10291845+szv@users.noreply.github.com>
2022-10-08 06:53:13 +0200

    Blazor: Added game page (based on existing ViewModels)

--- src/clients/blazor/CodeBreaker.Blazor/Program.cs
+++ src/clients/blazor/CodeBreaker.Blazor/Program.cs
@@ -6,6 +6,9 @@ using CodeBreaker.Services.Authentication;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using CodeBreaker.ViewModels;
+using CodeBreaker.ViewModels.Services;
+using CodeBreaker.Blazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,9 +17,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazorApplicationInsights();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IGameReportClient, GameClient>();
+builder.Services.AddScoped<IGameClient, GameClient>();
+builder.Services.AddMudServices();
+builder.Services.AddTransient<IDialogService, DialogService>();
+builder.Services.AddScoped<GamePageViewModel>();
 builder.Services.AddScoped(sp => new HttpClient {
     BaseAddress = new Uri(builder.Configuration["ApiBase"] ?? throw new InvalidOperationException("Missing ApiBase configuration"))
 });
-builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
---
+++ src/clients/blazor/CodeBreaker.Blazor/Services/DialogService.cs
@@ -0,0 +1,15 @@
+﻿using CodeBreaker.ViewModels.Services;
+
+namespace CodeBreaker.Blazor.Services;
+
+public class DialogService : IDialogService
+{
+    private readonly MudBlazor.IDialogService _mudBlazorDialogService;
+
+    public DialogService(MudBlazor.IDialogService mudBlazorDialogService)
+    {
+        _mudBlazorDialogService = mudBlazorDialogService;
+    }
+    public Task ShowMessageAsync(string message) =>
+        _mudBlazorDialogService.ShowMessageBox("CodeBreaker", message);
+}
