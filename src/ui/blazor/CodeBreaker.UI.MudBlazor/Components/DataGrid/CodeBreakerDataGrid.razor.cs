bce2ee652875c788de6ae40b78fd15a60ebe0615
Patrick Jahr <patrick.jahr@thinktecture.com>
2022-11-21 13:08:43 +0100

    chore: fix datagrid

--- src/ui/blazor/CodeBreaker.UI.MudBlazor/Components/DataGrid/CodeBreakerDataGrid.razor.cs
+++ src/ui/blazor/CodeBreaker.UI.MudBlazor/Components/DataGrid/CodeBreakerDataGrid.razor.cs
@@ -5,21 +5,9 @@ using MudBlazor;
 namespace CodeBreaker.UI;
 
 public partial class CodeBreakerDataGrid<T>
-{
-    [Parameter]
-    public List<string> Headers { get; set; } = new List<string>();
-
-    [Parameter]
-    public List<T> Items { get; set; } = new();
-
-    [Parameter]
-    public List<CodeBreakerColumnDefinition<T>> Columns { get; set; } = new List<CodeBreakerColumnDefinition<T>>();
-
+{    
     [Parameter]
     public RenderFragment<T> RowContent { get; set; } = default!;
-
-    [Parameter]
-    public EventCallback<T> RowItemClicked { get; set; } = new();
 
     private async Task OnRowClicked(TableRowClickEventArgs<T> row)
     {
