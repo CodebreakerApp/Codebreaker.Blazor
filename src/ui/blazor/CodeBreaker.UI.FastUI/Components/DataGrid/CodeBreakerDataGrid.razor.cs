99d3d0346b77c33bc8a3a431cbe726432097e216
Patrick Jahr <patrick.jahr@thinktecture.com>
2022-11-28 07:44:26 +0100

    chore: enable color selection if field is selected

--- src/ui/blazor/CodeBreaker.UI.FastUI/Components/DataGrid/CodeBreakerDataGrid.razor.cs
+++ src/ui/blazor/CodeBreaker.UI.FastUI/Components/DataGrid/CodeBreakerDataGrid.razor.cs
@@ -1,24 +1,9 @@
-using System.Text.Json;
-using CodeBreaker.UI.Shared.Models.DataGrid;
-using Microsoft.AspNetCore.Components;
 using Microsoft.Fast.Components.FluentUI;
 
 namespace CodeBreaker.UI;
 
 public partial class CodeBreakerDataGrid<T>
 {
-    [Parameter]
-    public List<string> Headers { get; set; } = new List<string>();
-
-    [Parameter]
-    public List<T> Items { get; set; } = new();
-
-    [Parameter]
-    public List<CodeBreakerColumnDefinition<T>> Columns { get; set; } = new List<CodeBreakerColumnDefinition<T>>();
-
-    [Parameter]
-    public EventCallback<T> RowItemClicked { get; set; } = new();
-
     private List<ColumnDefinition<T>> _columnDefinitions = new();
     private string _columnTemplate = string.Empty;
 
@@ -33,21 +18,9 @@ public partial class CodeBreakerDataGrid<T>
 
     private async Task OnRowClicked(FluentDataGridRow<T> row)
     {
-        if (row.RowIndex.HasValue)
+        if (row.RowData != null)
         {
-            try
-            {
-                var rowIndex = row.RowIndex.Value > 0 ? row.RowIndex.Value : 1;
-                var data = Items[rowIndex - 1];
-                if (data != null)
-                {
-                    await RowItemClicked.InvokeAsync(data);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Row {row.RowIndex}, ItemsCount: {Items.Count}, Error: {ex.Message}");
-            }
+            await RowItemClicked.InvokeAsync(row.RowData);
         }
     }
 }
