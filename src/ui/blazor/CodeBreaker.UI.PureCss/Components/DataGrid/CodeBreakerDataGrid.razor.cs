2f048bf514d913d0cbb04136eea5ff549e84b0c7
Patrick Jahr <patrick.jahr@thinktecture.com>
2023-01-04 11:45:13 +0100

    chore: add QuickGird

---
+++ src/ui/blazor/CodeBreaker.UI.PureCss/Components/DataGrid/CodeBreakerDataGrid.razor.cs
@@ -0,0 +1,16 @@
+namespace CodeBreaker.UI;
+
+public partial class CodeBreakerDataGrid<T>
+{
+    private IQueryable<T> source;
+    protected override void OnInitialized()
+    {
+        source = Items.AsQueryable();
+        base.OnInitialized();
+    }
+
+    private async Task Hire(T context) 
+    {
+        await RowItemClicked.InvokeAsync(context);
+    }
+}
