2d2d7fbf12e5c8454f2de562db5705ee70baa771
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-02-05 19:49:11 +0100

    Used file-scoped namepsaces

--- src/clients/blazor/CodeBreaker.Blazor/Pages/Authentication.razor.cs
+++ src/clients/blazor/CodeBreaker.Blazor/Pages/Authentication.razor.cs
@@ -1,19 +1,17 @@
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
-namespace CodeBreaker.Blazor.Pages
+namespace CodeBreaker.Blazor.Pages;
+
+public partial class Authentication
 {
-    public partial class Authentication
+    [Inject]
+    private NavigationManager NavigationManager { get; init; } = default!;
+
+    [Parameter]
+    public string Action { get; set; } = string.Empty;
+
+    private void OnLogout()
     {
-        [Inject]
-        private NavigationManager NavigationManager { get; init; } = default!;
-
-        [Parameter]
-        public string Action { get; set; } = string.Empty;
-
-        private void OnLogout()
-        {
-            NavigationManager.NavigateTo("/", true, true);
-        }
+        NavigationManager.NavigateTo("/", true, true);
     }
 }
